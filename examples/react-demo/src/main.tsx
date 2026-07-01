5a54ecd419c04714c93a05de00d54127e6409b74
Sam Vervaeck <samvv@pm.me>
2026-07-01 22:37:05 +0200

    Add clickable parameter to React demo

--- examples/react-demo/src/main.tsx
+++ examples/react-demo/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { FocusGroup, keyboard, Manager, NavigationProvider, useFocusable, type Mode } from '@samvv/nav-react'
 
@@ -10,12 +10,14 @@ type RectangleProps = {
   color: string;
   mode?: Mode;
   defaultFocused?: boolean;
+  clickable?: boolean;
 }
 
-function Rectangle({ mode, color, defaultFocused, x, y, w, h }: RectangleProps) {
+function Rectangle({ mode, clickable, color, defaultFocused, x, y, w, h }: RectangleProps) {
   const { ref, focused } = useFocusable<HTMLDivElement>({
     mode,
     defaultFocused,
+    clickable,
   });
   return (
     <div ref={ref} style={{
@@ -41,20 +43,28 @@ function nextMode(mode: Mode): Mode {
   }
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+function App() {
+  const [clickable, setClickable] = useState(true);
+  return (
     <NavigationProvider manager={manager}>
       <button onClick={() => manager.setMode(nextMode(manager.getMode()))}>Switch Mode</button>
-      <Rectangle defaultFocused color="red" x={100} y={50} w={100} h={50} />
-      <Rectangle color="red" x={300} y={25} w={50} h={50} />
-      <Rectangle color="red" x={100} y={250} w={100} h={50} />
-      <Rectangle color="red" x={300} y={225} w={50} h={50} />
+      <button onClick={() => setClickable(!clickable)}>Change Clickable</button>
+      <Rectangle clickable={clickable} defaultFocused color="red" x={100} y={50} w={100} h={50} />
+      <Rectangle clickable={clickable} color="red" x={300} y={25} w={50} h={50} />
+      <Rectangle clickable={clickable} color="red" x={100} y={250} w={100} h={50} />
+      <Rectangle clickable={clickable} color="red" x={300} y={225} w={50} h={50} />
       <FocusGroup mode={1}>
-        <Rectangle color="blue" defaultFocused x={100} y={450} w={100} h={50} />
-        <Rectangle color="blue" x={300} y={425} w={50} h={50} />
-        <Rectangle color="blue" x={100} y={650} w={100} h={50} />
-        <Rectangle color="blue" x={300} y={625} w={50} h={50} />
+        <Rectangle clickable={clickable} color="blue" defaultFocused x={100} y={450} w={100} h={50} />
+        <Rectangle clickable={clickable} color="blue" x={300} y={425} w={50} h={50} />
+        <Rectangle clickable={clickable} color="blue" x={100} y={650} w={100} h={50} />
+        <Rectangle clickable={clickable} color="blue" x={300} y={625} w={50} h={50} />
       </FocusGroup>
     </NavigationProvider>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
   </StrictMode>,
 )
