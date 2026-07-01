0a1bdc1607e8d038eae2412088e751cb1639dc2d
Sam Vervaeck <samvv@pm.me>
2026-07-01 22:29:54 +0200

    Add segment/segment intersection logic
    It must be kept somewhere

--- core/src/shapes.ts
+++ core/src/shapes.ts
@@ -272,3 +272,15 @@ export function isIntersectionLineAABB(line: Line, b: AABB): boolean {
     }
   }
 }
+
+export function intersectLineLine([[x1, y1], [x2, y2]]: Line, [[x3, y3], [x4, y4]]: Line): Vec2 | undefined {
+  const k = ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+  const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / k;
+  const u = - ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / k;
+  if (0 <= t && t <= 1 && u <= 0 && u <= 1) {
+    return [
+      x1 + t * (x2 - x1),
+      y1 + t * (y2 - y1),
+    ];
+  }
+}
